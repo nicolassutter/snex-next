2e3e3e9334a7ac40a7743d0e34c0e26d5d239a42
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2022-12-11 21:25:45 +0100

    feat: display movie list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
+import type { Movie } from '../../types'
+import { PosterCard } from '../components/PosterCard'
+import { api } from '../modules/api'
+
 function Index() {
-  return <div className="index"></div>
+  const result = useSignal<Movie[]>([])
+
+  useEffectOnce(async () => {
+    const {
+      data: { results },
+    } = await api.getPopularMovies()
+
+    result.value = results
+  })
+
+  return (
+    <div className='index-page'>
+      {result.value.map((v) => (
+        <PosterCard
+          key={v.id}
+          src={v.poster_path}
+          imgAttrs={{
+            className: 'hello',
+          }}
+        ></PosterCard>
+      ))}
+    </div>
+  )
 }
 
 export default Index
