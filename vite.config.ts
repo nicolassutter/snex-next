8cd9b05c6f0e820dd1f1d615112995d5c650e41f
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2022-12-12 10:08:56 +0100

    chore: add unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,29 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Pages from 'vite-plugin-pages'
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '#types',
+        replacement: './types',
+      },
+      {
+        find: '#src',
+        replacement: './src',
+      },
+    ],
+  },
   plugins: [
     preact(),
+    Icons({
+      autoInstall: true,
+      compiler: 'jsx',
+      jsx: 'preact',
+    }),
     AutoImport({
       imports: [
         'preact',
