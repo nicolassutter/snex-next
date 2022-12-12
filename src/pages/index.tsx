fcaab71c8899a65df241e364d02e285531a25336
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2022-12-12 15:56:23 +0100

    refactor: better results handling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,28 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 
 function Index() {
-  const results = useSignal({
-    top_rated: [] as Movie[],
-    popular: [] as Movie[],
-  })
+  const results = useSignal([
+    {
+      key: 'top_rated',
+      label: 'Top rated movies',
+      items: [] as Movie[],
+    } as const,
+    {
+      key: 'popular',
+      label: 'Popular movies',
+      items: [] as Movie[],
+    } as const,
+  ])
+
+  function updateResults(key: string, newValue: Movie[]) {
+    const resultIndex = results.value.findIndex((res) => res.key === key)
+
+    if (resultIndex !== -1) {
+      const resultsCopy = JSON.parse(JSON.stringify(results.value))
+      resultsCopy[resultIndex].items = newValue
+      results.value = resultsCopy
+    }
+  }
 
   useEffectOnce(() => {
     makePromise(async () => {
@@ -18,29 +36,26 @@ function Index() {
         api.getTopMovies(),
       ])
 
-      results.value = {
-        ...results.value,
-        popular: popularResults.data?.results ?? [],
-        top_rated: topResults.data?.results ?? [],
-      }
+      updateResults('popular', popularResults.data?.results ?? [])
+      updateResults('top_rated', topResults.data?.results ?? [])
     })
   })
 
   return (
     <div className='index-page p-2'>
-      {Object.entries(results.value).map(([key, value]) => (
+      {results.value.map((result) => (
         <div
           className='banner mt-5 first:mt-0'
-          key={key}
+          key={result.key}
         >
-          <h2 className='text-3xl font-bold'>{key}</h2>
+          <h2 className='text-3xl font-bold'>{result.label}</h2>
 
           <Swiper
             spaceBetween={25}
             slidesPerView={7}
             className='mt-5'
           >
-            {value.map((movie) => (
+            {result.items.map((movie) => (
               <SwiperSlide
                 key={movie.id}
                 className='h-[unset] w-28'
