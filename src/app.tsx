03e6291dcce8530801c405bb73176526af6e23e7
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2022-12-14 17:49:32 +0100

    fix: make lazy loaded routes work

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,15 @@ import { useRoutes } from 'react-router-dom'
 import routes from '~react-pages'
 import { NavBar } from './components/NavBar'
 import './app.css'
+import { Suspense } from 'preact/compat'
 
 export function App() {
   return (
     <>
       <NavBar></NavBar>
-      <main role="main">{useRoutes(routes)}</main>
+      <main role='main'>
+        <Suspense fallback={<></>}>{useRoutes(routes)}</Suspense>
+      </main>
     </>
   )
 }
