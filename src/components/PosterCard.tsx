5ada447bb430d43bc5de5f97e2a356fe1f13f170
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2022-12-21 09:31:30 +0100

    feat: remove loader if image loaded

--- src/components/PosterCard.tsx
+++ src/components/PosterCard.tsx
@@ -19,6 +19,7 @@ export const PosterCard: FunctionComponent<Props> = ({
 }) => {
   const img = useRef<HTMLImageElement | null>(null)
   const useLazy = lazy === true || lazy === undefined
+  const [hasLoaded, setHasLoaded] = useState(false)
 
   // If we lazy load the image: false by default
   // If we do not lazy load the image: true by default
@@ -35,29 +36,31 @@ export const PosterCard: FunctionComponent<Props> = ({
   }
 
   return (
-    <div className={clsx('poster-card relative', className)}>
-      <span className='card-loader aspect-[2/3] absolute z-10 w-full rounded-md flex items-center justify-center bg-base-200'>
-        <svg
-          className='animate-spin h-5 w-5 text-info'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-        >
-          <circle
-            className='opacity-25'
-            cx='12'
-            cy='12'
-            r='10'
-            stroke='currentColor'
-            strokeWidth='4'
-          ></circle>
-          <path
-            className='opacity-75'
-            fill='currentColor'
-            d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-          ></path>
-        </svg>
-      </span>
+    <div className={clsx('poster-card relative aspect-[2/3]', className)}>
+      {!hasLoaded && (
+        <span className='card-loader aspect-[2/3] absolute z-10 w-full rounded-md flex items-center justify-center bg-base-200'>
+          <svg
+            className='animate-spin h-5 w-5 text-info'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+          >
+            <circle
+              className='opacity-25'
+              cx='12'
+              cy='12'
+              r='10'
+              stroke='currentColor'
+              strokeWidth='4'
+            ></circle>
+            <path
+              className='opacity-75'
+              fill='currentColor'
+              d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+            ></path>
+          </svg>
+        </span>
+      )}
 
       <img
         ref={img}
@@ -69,6 +72,7 @@ export const PosterCard: FunctionComponent<Props> = ({
         src={hasIntersected ? src : undefined}
         alt={alt}
         draggable={false}
+        onLoad={() => setHasLoaded(true)}
         // loading='lazy' does not work
       />
     </div>
