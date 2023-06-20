fbe45b6b6e23229ff43ac4d2148b45609518e936
nicolassutter <contact.sutter.nicolas@gmail.com>
2023-06-20 15:13:17 +0200

    feat: display more items on big displays on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,7 +69,7 @@ function Index() {
                             key={item.id}
                             className={clsx(
                               'swiper-poster-slide shrink-0 grow-0',
-                              'w-full xxs:w-1/2 sm:w-1/4 lg:w-1/5',
+                              'w-full xxs:w-1/2 sm:w-1/4 lg:w-1/5 2xl:w-1/6',
                             )}
                           >
                             <Link to={`/media/${category}/${item.id}`}>
